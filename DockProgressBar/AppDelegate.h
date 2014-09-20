dcd6e7d629dbe508821bc25d936a42b8ac5cead0
Haojian Wu <hokein.wu@gmail.com>
2014-09-20 16:48:54 +0800

    Update sample app.

--- DockProgressBar/AppDelegate.h
+++ DockProgressBar/AppDelegate.h
@@ -3,5 +3,12 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSPopUpButton *progressBarList;
+@property (weak) IBOutlet NSTextField *percentField;
+@property (weak) IBOutlet NSSlider *percentSlider;
+
+- (IBAction)showProgressBar:(id)sender;
+- (IBAction)hideProgressBar:(id)sender;
+- (IBAction)OnValueChange:(id)sender;
 
 @end
--- DockProgressBar/AppDelegate.m
+++ DockProgressBar/AppDelegate.m
@@ -1,14 +1,61 @@
 #import "AppDelegate.h"
 #import "DockDownloadProgressBar.h"
+#import "DockCircularProgressBar.h"
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-  DockDownloadProgressBar* progress_bar =
-      [DockDownloadProgressBar sharedDockDownloadProgressBar];
-  [progress_bar setProgress:0.3];
-  [progress_bar updateProgressBar];
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [[self progressBarList] removeAllItems];
+  NSArray* types_progress_bar = @[@"DownloadProgressBar",
+                                  @"CircularProgressBar"];
+  [[self progressBarList] addItemsWithTitles:types_progress_bar];
+  [[self progressBarList] selectItemAtIndex:0];
+
+  // Show percent.
+  NSNumberFormatter* formatter = [[NSNumberFormatter alloc] init];
+  NSString* percentString =
+     [formatter stringFromNumber: [NSNumber numberWithInt:
+         [[self percentSlider] intValue]]];
+
+  [[self percentField] setStringValue:percentString];
+}
+
+- (IBAction)showProgressBar:(id)sender {
+  double progress = [[self percentSlider] intValue]/100.0;
+
+  // Make sure the previous ProgressBar is clear before update.
+  [[DockDownloadProgressBar sharedDockDownloadProgressBar] clear];
+  [[DockCircularProgressBar sharedDockCircularProgressBar] clear];
+
+  NSString* selected_title = [[[self progressBarList] selectedItem] title];
+  if ([selected_title isEqual: @"DownloadProgressBar"]) {
+    [[DockDownloadProgressBar sharedDockDownloadProgressBar]
+        setProgress:progress];
+    [[DockDownloadProgressBar sharedDockDownloadProgressBar] updateProgressBar];
+  } else if ([selected_title isEqual: @"CircularProgressBar"]) {
+    [[DockCircularProgressBar sharedDockCircularProgressBar]
+        setProgress:progress];
+    [[DockCircularProgressBar sharedDockCircularProgressBar] updateProgressBar];
+  }
+}
+
+- (IBAction)hideProgressBar:(id)sender {
+  NSString* title = [[[self progressBarList] selectedItem] title];
+  if ([title isEqual: @"DownloadProgressBar"]) {
+    [[DockDownloadProgressBar sharedDockDownloadProgressBar] clear];
+  } else if ([title isEqual: @"CircularProgressBar"]) {
+    [[DockCircularProgressBar sharedDockCircularProgressBar] clear];
+  }
+ }
+
+- (IBAction)OnValueChange:(id)sender {
+  // Show percent.
+  NSNumberFormatter* formatter = [[NSNumberFormatter alloc] init];
+  NSString* percentString =
+  [formatter stringFromNumber:[NSNumber numberWithInt:
+      [[self percentSlider] intValue]]];
+
+  [[self percentField] setStringValue:percentString];
 }
 
 @end
